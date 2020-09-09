c61be91041ff5ac66d4f4f00136324d9b47448c5
Jacob Fry <jacob.fry@sydney.edu.au>
2020-09-09 18:35:59 +0900

    Added more table specifications

---
+++ join_countries.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+a.to_csv('xgboost.txt', header=True, index=False, sep='\t', mode='a')
--- make_requests.py
+++ make_requests.py
@@ -15,9 +15,8 @@ if not os.path.isdir(save_dir):
     os.mkdir(save_dir)
 
 # URL components
-base_url_snippet = 'http://data.un.org/Handlers/DownloadHandler.ashx?'
-end_url_snippet = '&Format=csv&c=2,3,4,6,7,8,9,10,11,12,13&s=_cr_engNameOrderBy:asc,fiscal_year:desc,_grIt_code:asc'
-# end_url_snippet = '&Format=csv&c=2,3,4,6,7,8,9,10,11,12,13,14&s=_cr_engNameOrderBy:asc,fiscal_year:desc,_grIt_code:asc'
+base_url_snippet = 'http://data.un.org/Handlers/DownloadHandler.ashx?&Format=csv&'
+end_url_snippet = '&s=_cr_engNameOrderBy:asc,fiscal_year:desc,_grIt_code:asc'
 
 # Retrieve list of UN 2-digit country codes
 print('Getting UN country codes')
@@ -25,13 +24,36 @@ print('Getting UN country codes')
 country_legend = pd.read_excel(os.environ['legend_dir'] + 'RootRegionLegend.xlsx', sheet_name='RootCountryLegend')
 un_country_codes = country_legend['UNCode'].values
 
-# Options
+# Timeseries options
 year_start = 1990
 year_end = 2018
 year_rng = range(year_start, year_end)
 all_year_strings = [str(x) for x in list(year_rng)]
 
-table_rng = [101]
+# UN Table options
+table_optons = [{'tbl_id': 101, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tbl_id': 102, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13,14'}
+                , {'tbl_id': 103, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tbl_id': 201, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 202, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13,14'}
+                , {'tble_id': 203, 'col_specs': 'c=2,3,5,6,8,9,10,11,12,13,14,15'}
+                , {'tble_id': 204, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 205, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13,14'}
+                , {'tble_id': 206, 'col_specs': 'c=2,3,5,6,8,9,10,11,12,13,14,15'}
+                , {'tble_id': 301, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 302, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 401, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 402, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 403, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 404, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 405, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 406, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 407, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 408, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 409, 'col_specs': 'c=2,3,4,6,7,8,9,10,11,12,13'}
+                , {'tble_id': 501, 'col_specs': 'c=2,3,5,6,8,9,10,11,12,13,14,15'}
+                , {'tble_id': 502, 'col_specs': 'c=2,3,5,6,8,9,10,11,12,13,14,15'}
+                ]
 
 # Create a store to remember requests
 print('Creating tinyDB')
@@ -42,33 +64,47 @@ db = TinyDB(work_dir + 'request_store.json')
 # Loop over options and perform requests
 print('Scraping...')
 
-for un_table_id in table_rng:
+for un_table in table_optons:
+
+    un_table_id = un_table['tbl_id']
+
+    print('Table ' + str(un_table_id))
 
     # Create db table to match un table
     db_table = db.table('tbl' + str(un_table_id))
+
+    # Output directory
+    scraped_dir = save_dir + 'table' + str(un_table_id) + '/'
+    if not os.path.isdir(scraped_dir):
+        os.mkdir(scraped_dir)
 
     for country_code in un_country_codes:
 
         # 3 digit country name
         country_acronym = country_legend[country_legend['UNCode'] == country_code]['Root country abbreviation'].values[0]
-        print('Processing ' + country_acronym)
 
         # Download if file does not yet exist
         previous_request = db_table.search(Query()['country'] == country_code)
 
         if not previous_request:
 
+            print(' Downloading ' + country_acronym)
+
+            # Wait between requests so as to not abuse the UN API
+            time.sleep(10)
+
             # Construct request url
-            data_filter = ('DataFilter=group_code:' + str(un_table_id) + ';country_code:' + str(country_code)
-                           + ';fiscal_year:' + ','.join(all_year_strings) + '&DataMartId=SNA')
-            csv_url = base_url_snippet + data_filter + end_url_snippet
+            csv_url = (base_url_snippet + 'DataFilter=group_code:' + str(un_table_id)
+                       + ';country_code:' + str(country_code) + ';fiscal_year:' + ','.join(all_year_strings)
+                       + '&DataMartId=SNA&Format=csv&' + un_table['col_specs'] + end_url_snippet)
 
             # Make request
             req = requests.get(csv_url, allow_redirects=True)
-            status_code = req.status_code
-            if status_code == 200:  # check is response is OK
 
-                result_fname = (save_dir + 'UNOC_' + country_acronym + '_' + str(country_code)
+            status_code = req.status_code
+            if status_code == 200:  # response is OK
+
+                result_fname = (scraped_dir + 'UNOC_' + country_acronym + '_' + str(country_code)
                                 + '_table' + str(un_table_id) + '_' + str(year_start) + '-' + str(year_end))
 
                 # Unpack the response
@@ -111,9 +147,12 @@ for un_table_id in table_rng:
                 os.remove(result_fname + '.zip')
 
             else:
-                print('Request failed (' + str(status_code) + ') for ' + data_filter)
+                print('Request failed (' + str(status_code) + ') for table' + str(un_table_id) + 'country'
+                      + str(country_code))
 
-        # Wait between requests so as to not abuse the UN API
-        time.sleep(10)
+    any_successful_requests = db_table.search(Query()['success'] == 1)
+    assert any_successful_requests is not []
+
+    print('.')
 
 print('All finished')
